325a1a112fdca85b6a2b37d85a7f83951fe4a65e
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-10-08 15:52:39 -0400

    Alternar baseado no checkbox

---
+++ custom.js
@@ -0,0 +1,13 @@
+/* Troca de Campos de Materiais */
+
+$(document).ready(function() {
+    $('#Divulgacao').change(function() {
+        $('#campo-divulgacao').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('#Apoio').change(function() {
+        $('#campo-apoio').toggle();
+    });
+});
